9ca24f84109e94040bdb8d41e274e5d78814e568
kfegert <fegert@seracom.de>
2013-08-13 13:57:25 +0200

    Revert "Fixed issue #08033: since enable CSRF protection: Cannot access advanced question attributes"
    This reverts commit aeff90fbbf2f05687637680fe9ef0b14f0a9d029.

--- application/views/admin/survey/Question/editQuestion_view.php
+++ application/views/admin/survey/Question/editQuestion_view.php
@@ -1,8 +1,6 @@
 <script type='text/javascript'>
-    var attr_url = "<?php echo $this->createUrl('/admin/questions/ajaxquestionattributes'); ?>";
+    var attr_url = "<?php echo $this->createUrl('/admin/questions/sa/ajaxquestionattributes'); ?>";
     var imgurl = '<?php echo Yii::app()->getConfig('imageurl'); ?>';
-    var yii_csrf = "<?php echo Yii::app()->request->csrfToken; ?>";
-    
 </script>
 <?php PrepareEditorScript(true, $this); ?>
 
